5cbf0d364aff97e8054c589660e88e54562ec4bf
Toshihito Waki <wakit@mtf.biglobe.ne.jp>
2025-07-30 19:49:03 +0900

    Option to exclude the last page when selecting images

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -50,7 +50,7 @@ class SettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('iiif_random_block.settings');
 
-    // Source Settings...
+    // Source Settings.
     $form['source_settings'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Source Information'),
@@ -67,7 +67,7 @@ class SettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('source_link_url'),
     ];
 
-    // Display Settings...
+    // Display Settings.
     $form['display_settings'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Display Settings'),
@@ -91,11 +91,11 @@ class SettingsForm extends ConfigFormBase {
     ];
 
     // Image Selection Rules.
-    $default_rules = "1 => 1\n2 => 2\n3 => 3\n4+ => random(3-last)";
+    $default_rules = "1 => 1\n2 => 2\n3+ => random(2-last-1)";
     $form['selection_rules_settings'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Image Selection Rules'),
-      '#description' => $this->t('Define rules for selecting an image based on the total number of canvases in a manifest. The first matching rule from the top will be used. If no rules match, a random image from all canvases will be selected.'),
+      '#description' => $this->t('Define rules for selecting an image based on the total number of canvases in a manifest. The first matching rule from the top will be used.'),
     ];
     $form['selection_rules_settings']['selection_rules'] = [
       '#type' => 'textarea',
@@ -116,12 +116,12 @@ class SettingsForm extends ConfigFormBase {
                 <li><code>last</code>: Select the very last canvas.</li>
                 <li><code>random</code>: Select a random canvas from all available.</li>
                 <li><code>random(2-last)</code>: Select a random canvas from the 2nd to the last.</li>
-                <li><code>random(1-5)</code>: Select a random canvas from the 1st to the 5th.</li>
+                <li><code>random(1-last-1)</code>: Select a random canvas from the 1st to the second to last (excludes the last page).</li>
             </ul>
         '),
     ];
 
-    // Cron Settings...
+    // Cron Settings.
     $form['cron_settings'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Update Frequency'),
@@ -133,7 +133,7 @@ class SettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('cron_interval') ?: 86400,
     ];
 
-    // Manifest URL list...
+    // Manifest URL list.
     $query = $this->database->select('iiif_manifest_urls', 'm')->fields('m', ['url']);
     $results = $query->execute()->fetchCol();
     $urls = implode("\n", $results);
@@ -166,7 +166,7 @@ class SettingsForm extends ConfigFormBase {
       ->set('cron_interval', $form_state->getValue('cron_interval'))
       ->save();
 
-    // Save manifest URLs...
+    // Save manifest URLs.
     $urls_text = $form_state->getValue('manifest_urls');
     $urls = preg_split('/\\r\\n|\\r|\\n/', $urls_text, -1, PREG_SPLIT_NO_EMPTY);
     $urls = array_unique(array_filter($urls, fn($url) => filter_var(trim($url), FILTER_VALIDATE_URL)));
